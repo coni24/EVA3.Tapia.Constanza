c1eee63c7b93bf0f9b605dc64b4241aeb51404bc
coni24 <169170065+coni24@users.noreply.github.com>
2024-06-12 11:29:12 -0400

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,5 +18,50 @@ document.getElementById('Contraste').addEventListener("click",function(){
 })
 
 function validar(){
-    eValidar('nombre');
+    validarCampo("nombre");
+    validarCampo("email");
+    validarCampo("asunto");
+    validarCampo("mensaje");
+    validarTelefono("telefono");
+}
+
+function validarCampo(idCampo){
+    let elemento = document.getElementById(idCampo);
+    let valor = elemento.value;
+    let eParrafo = document.getElementById("p" + idCampo);
+
+    if(valor.trim() == ""){
+        console.log("Nada")
+        elemento.style.borderColor = "pink";
+        eParrafo.style.display = "block";
+    }else{
+        let contieneNumero = /[0-9]/.test(valor);
+        if(contieneNumero) {
+            elemento.style.borderColor = "pink";
+            eParrafo.innerText = "No puede contener numeros";
+            eParrafo.style.display = "block";
+        }else{
+        console.log("algo")
+        elemento.style.borderColor = "green";
+        eParrafo.style.display = "none";
+        }
+    }
+}
+  
+function validarTelefono(idCampo){
+    let elemento = document.getElementById(idCampo);
+    let valor = elemento.value;
+    let eParrafo = document.getElementById("p" + idCampo);
+
+    if (isNaN(valor)|| Number(valor)<0){
+        eParrafo.innerText = "Debes ingresar un numero valido";
+        eParrafo.style.display = "block";
+    }else if (valor.trim().length === 9 || valor.trim().length === 0){
+        elemento.style.borderColor = "green";
+        eParrafo.style.display = "none";
+    }else {
+        elemento.style.borderColor = "pink";
+        eParrafo.innerText = "Debes ingresar un numero de 9 digitos";
+        eParrafo.style.display = "block";
+    }
 }
